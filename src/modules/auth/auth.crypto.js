c87828c2a1085008f558c6b0a08cba245a016fa7
w245607930 <245607930@qq.com>
2026-09-08 18:13:31 +0800

    test(auth): cover crypto and harden production secrets

--- src/modules/auth/auth.crypto.js
+++ src/modules/auth/auth.crypto.js
@@ -1,16 +1,17 @@
 import crypto from 'node:crypto';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
+import { env } from '../../config/env.js';
 
 export const hashPassword = (password) => bcrypt.hash(password, 12);
 export const verifyPassword = (password, hash) => bcrypt.compare(password, hash);
 export function signAccessToken(user) {
-  return jwt.sign({ sub: user.id, tv: user.tokenVersion }, process.env.JWT_ACCESS_SECRET, {
-    expiresIn: process.env.JWT_ACCESS_TTL ?? '15m',
+  return jwt.sign({ sub: user.id, tv: user.tokenVersion }, env.JWT_ACCESS_SECRET, {
+    expiresIn: env.JWT_ACCESS_TTL,
   });
 }
 export function verifyAccessToken(token) {
-  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+  return jwt.verify(token, env.JWT_ACCESS_SECRET);
 }
 export const createRefreshToken = () => crypto.randomBytes(48).toString('base64url');
 export const hashRefreshToken = (token) => crypto.createHash('sha256').update(token).digest('hex');
